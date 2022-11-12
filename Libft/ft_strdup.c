72c4b6cae87d0d744020112041deaecb87724374
Hakan UGUR <ugur_hakan@yahoo.fr>
2022-11-12 22:11:24 +0100

    modif strdup

--- Libft/ft_strdup.c
+++ Libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: hugur <hugur@42lausanne.ch>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 15:56:39 by hugur             #+#    #+#             */
-/*   Updated: 2022/11/04 12:00:23 by hugur            ###   ########.fr       */
+/*   Updated: 2022/11/12 22:08:03 by hugur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*ft_strdup(const char *s1)
 	ptr = malloc(sizeof(char) * ft_strlen(s1) + 1);
 	if (!(ptr))
 		return (NULL);
-		i = 0;
+	i = 0;
 	while (s1[i])
 	{
 		ptr[i] = s1[i];
