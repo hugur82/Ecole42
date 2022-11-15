93f49d84cd56fdda436f450a7d181e3b1d6097b1
Hakan UGUR <ugur_hakan@yahoo.fr>
2022-11-15 21:48:31 +0100

    libftprintf added

--- printf/includes/libftprintf.h
+++ printf/includes/libftprintf.h
@@ -2,6 +2,8 @@
 # define LIBFTPRINTF_H
 
 # include <stdarg.h>
-#include <unistd.h>
-
+# include <unistd.h>
+int	ft_printchar(int c);
+int	ft_printstring(const char *str);
+int	ft_printint(int	nb);
 #endif
